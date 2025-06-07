3e1513866626ce8b589e251802129c34cd2f8bab
mohab ahmed zaher <mohabahmedzaher@gmail.com>
2025-06-08 02:59:00 +0300

    Delete python_introduction directory

--- python_introduction/basic_operations.py
+++
@@ -1,8 +0,0 @@
-number1 = 10
-number2 = 5
-addition=number1+number2
-Subtraction=number1-number2
-Multiplication=number1*number2
-print("Addition of 10 and 5 is ",addition)
-print("Subtraction of 10 and 5 is ",Subtraction)
-print("Multiplication of 10 and 5 is ",Multiplication)
